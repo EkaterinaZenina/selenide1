213e05867286d6505e44d6dc730763b6acdd30a4
Екатерина Зенина <zeninaekaterin@gmail.com>
2022-01-07 14:30:53 +0300

    update

--- src/test/java/ShouldRegByAccountTest.java
+++ src/test/java/ShouldRegByAccountTest.java
@@ -2,31 +2,32 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.*;
 
 public class ShouldRegByAccountTest {
+
+    public static String generateDate(int days){
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     @Test
     public void shouldRegByAccount() {
-
-        open("http://0.0.0.0:9999");
+     String planningDate = generateDate(4);
+        open("http://localhost:9999");
         $("[data-test-id='city'] .input__control").setValue("Вологда");
         $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] .input__control").setValue(String.valueOf(2022-01-10));
+        $("[data-test-id='date'] .input__control").setValue(String.valueOf(planningDate));
         $("[data-test-id='name'] .input__control").setValue("Евгения Солнцева");
         $x("//*[@name='phone']").setValue("+79117389427");
-        $("button").click();
         $(".checkbox__box").click();
         $(".button__text").click();
-        $("[data-test-id='notification']").shouldHave(Condition.text("Встреча успешно забронирована на " ), Duration.ofSeconds(15)).shouldBe(visible);
+        $("[data-test-id='notification']").shouldHave(Condition.text("Встреча успешно забронирована на "+ planningDate ), Duration.ofSeconds(15)).shouldBe(visible);
     }
 
 
