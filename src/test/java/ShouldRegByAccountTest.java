0b064f4cec4e89707b7317f918c18383178b8a09
Екатерина Зенина <zeninaekaterin@gmail.com>
2022-01-07 14:14:02 +0300

    add

--- src/test/java/ShouldRegByAccountTest.java
+++ src/test/java/ShouldRegByAccountTest.java
@@ -1,6 +1,7 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -18,6 +19,7 @@ public class ShouldRegByAccountTest {
 
         open("http://0.0.0.0:9999");
         $("[data-test-id='city'] .input__control").setValue("Вологда");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         $("[data-test-id='date'] .input__control").setValue(String.valueOf(2022-01-10));
         $("[data-test-id='name'] .input__control").setValue("Евгения Солнцева");
         $x("//*[@name='phone']").setValue("+79117389427");
